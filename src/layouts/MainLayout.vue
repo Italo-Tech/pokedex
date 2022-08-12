8a6f426288cb732d2b75202fa2e63c500ba4867d
italo <italo@appbee.online>
2022-08-12 11:49:24 -0300

    Código otimizado UX e UI

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,33 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title class="text-center">
-          Pokedex
+      <q-toolbar class="bg-secondary">
+        <q-toolbar-title class="text-center text-weight-bold">
+          Pokédex
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
+
+    <q-footer elevated class="bg-secondary shadow-2 text-white fixed-bottom">
+      <q-tabs
+        indicator-color="primary"
+        active-color="primary"
+        class="text-white"
+      >
+        <q-tab name="home" icon="home" label="home" class="text-capitalize" no-caps></q-tab>
+        <q-tab name="favorite" icon="favorite" label="favorite" class="text-capitalize" no-caps>
+           <q-badge color="red" floating>4</q-badge>
+        </q-tab>
+        <q-tab name="notification" icon="notifications_active" label="note" class="text-capitalize" no-caps>
+          <q-badge color="red" floating>2</q-badge>
+        </q-tab>
+        <q-tab name="event" icon="event" label="event" class="text-capitalize" no-caps>
+           <q-badge color="red" floating>2</q-badge>
+        </q-tab>
+        <q-tab name="settings" icon="settings" label="settings" class="text-capitalize" no-caps></q-tab>
+
+      </q-tabs>
+    </q-footer>
 
     <q-page-container>
       <router-view />
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,56 +1,60 @@
 <template>
   <q-page class="flex items-start justify-center q-pa-md">
     <div class="column flex-center">
-      <div class="column flex-center q-mb-xl">
+      <div class="column flex-center q-mb-xl text-grey-8">
         <span class="text-h5 q-pt-lg q-pb-md">Bem vindo ao Pokedex</span>
         <span class="text-subtitle1">Descubra novos pokemons pesquisando por seu nome</span>
       </div>
 
       <section>
         <q-card flat style="width: 300px;">
-          <div v-if="pokemonUrl" class="q-p-lg">
-            <q-img :src="pokemonUrl" fit="cover"/>
+          <div v-if="pokemon.url" class="q-p-lg">
+            <q-img :src="pokemon.url" fit="cover"/>
           </div>
           <q-img class="q-pa-lg" v-else src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png" fit="cover"/>
         </q-card>
 
-        <div v-if="pokemonId" class="full-width">
-          <div class="">
-            <span class="text-subtitle2 text-grey-7">Nº {{ pokemonId }}</span>
-          </div>
-          <div class="full-width q-mt-sm column">
-            <span class="text-h5 text-grey-10 text-capitalize">{{ pokemonName }}</span>
-          </div>
-          <div class="q-gutter-sm q-mt-xs">
-            <q-badge v-for="type in pokemonType" color="grey-7" :label="type.type.name" class="q-pa-sm text-capitalize"/>
-          </div>
+        <div v-if="pokemon.id">
+          <!--Id-->
+          <q-card-actions>
+            <span class="text-subtitle2 text-grey-7">Nº {{ pokemon.id }}</span>
+          </q-card-actions>
+          <!--Name-->
+          <q-card-actions>
+            <span class="text-h5 text-grey-10 text-capitalize">{{ pokemon.name }}</span>
+          </q-card-actions>
+          <!--Badge-->
+          <q-card-actions class="q-gutter-sm">
+            <q-badge v-for="what_type in pokemon.type" color="grey-7" :label="what_type.type.name" class="q-pa-sm text-capitalize"/>
+          </q-card-actions>
         </div>
       </section>
 
       <div class="row justify-center full-width q-mt-xl">
-        <!--<q-input filled v-model="pokemonSearch" label="Digite nome pokemon" />
-        <q-btn color="primary" label="pesquisar" @click="getPokemon"/>-->
-
         <q-select
           filled
           v-model="pokemonSearch"
-          use-input
+          class=""
           input-debounce="0"
+          color="secondary"
           label="Selecione um pokemon"
+          label-color="secondary"
+          transition-show="jump-up"
+          transition-hide="jump-up"
           :options="pokemonOptions"
-          @filter="filterFn"
+          clearable
           style="width: 250px"
           behavior="menu"
         >
           <template v-slot:no-option>
             <q-item>
               <q-item-section class="text-grey">
-                No results
+                Sem resultados
               </q-item-section>
             </q-item>
           </template>
         </q-select>
-        <q-btn color="primary" label="pesquisar" @click="getPokemon"/>
+        <q-btn :disable="!pokemonSearch" color="secondary" label="pesquisar" @click="getPokemon"/>
       </div>
     </div>
   </q-page>
@@ -58,96 +62,76 @@
 
 <script>
 import api from "../services/api"
-import { ref, onBeforeMount } from 'vue'
+import { ref } from 'vue'
 import { useQuasar } from "quasar";
 
 export default {
   setup() {
     const $q = useQuasar()
 
-    const pokemonId = ref('')
-    const pokemonName = ref('')
-    const pokemonType = ref([])
-    const pokemonUrl = ref('')/*https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg*/
-    const pokemonSearch = ref('')/*bulbasaur*/
-
-    const pokemonOptions = ref(['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree', 'weedle', 'beedrill', 'kakuna'])
-    const pokemonOptions2 = []
-    const options = ref(pokemonOptions)
-
-    onBeforeMount(() => {
-      getAllPokemon()
+    const pokemon = ref({
+      id: null,
+      name: null,
+      type: [],
+      url: null,
     })
+    const pokemonOptions = ref([
+      'golem', 'My dog', 'gyarados', 'snorlax',
+      'steelix', 'hariyama', 'charizard',
+      'aggron', 'wailord', 'glalie', 'metagross',
+      'regirock', 'kyogre', 'groudon', 'torterra',
+      'rhyperior', 'mamoswine', 'dialga', 'palkia',
+      'heatran', 'spearow', 'raticate', 'rattata',
+      'pidgeot', 'pidgeotto', 'pidgey', 'beedrill'
+    ])
+    const pokemonSearch = ref(null)
+    const options = (pokemonOptions)
 
     const getPokemon = () => {
       api.get(`/pokemon/${pokemonSearch.value}/`)
         .then((response) => {
-          console.log(response);
-          console.log(response.data.types);
-          pokemonId.value = response.data.id
-          pokemonName.value = response.data.name
-          pokemonType.value = response.data.types
-          pokemonUrl.value = response.data.sprites.other.dream_world.front_default
+          pokemon.value.id = response.data.id
+          pokemon.value.name = response.data.name
+          pokemon.value.type = response.data.types
+          pokemon.value.url = response.data.sprites.other.dream_world.front_default
           triggerPositive()
         })
         .catch((error) => {
           triggerNegative()
         })
     }
-    const getAllPokemon = () => {
-      api.get('/pokemon?limit=100000&offset=0')
-        .then((response) => {
-          console.log(response.data.results);
-          pokemonOptions2.value = response.data.results
-          console.log(pokemonOptions2);
-        })
-    }
     const triggerPositive = () => {
       $q.notify({
-        type: 'positive',
-        position: 'top',
-        timeout: 1000,
-        message: 'Pokemon encontrado'
+        message: 'Pokemon encontrado!',
+        color: 'positive',
+        progress: true,
+        avatar: 'https://img.quizur.com/f/img5e5545da203841.94955565.png?lastEdited=1582646751',
+        actions: [
+          { label: 'Fechar', color: 'white', handler: () => { /* ... */ } }
+        ]
       })
     }
     const triggerNegative = () => {
       $q.notify({
-        type: 'warning',
-        timeout: 1000,
-        position: 'top',
-        message: 'Pokemon não encontrado, tente novamente'
+        message: 'Pokémon não encontrado, tente novamente!',
+        progress: true,
+        color: 'negative',
+        avatar: 'http://pa1.narvii.com/6431/79d57176492e24604caff0845a06d7f90b5e6d3f_00.gif',
+        actions: [
+          { label: 'Fechar', color: 'white', handler: () => { /* ... */ } }
+        ]
       })
     }
 
-
     return {
+      pokemon,
       pokemonSearch,
-      pokemonName,
-      pokemonUrl,
-      pokemonId,
-      pokemonType,
-      triggerPositive,
-      triggerNegative,
-      getPokemon,
-      getAllPokemon,
-
       pokemonOptions,
-      pokemonOptions2,
       options,
 
-      filterFn (val, update) {
-        if (val === '') {
-          update(() => {
-            options.value = pokemonOptions.value
-          })
-          return
-        }
-
-        update(() => {
-          const needle = val.toLowerCase()
-          options.value = pokemonOptions.value.filter(v => v.toLowerCase().indexOf(needle) > -1)
-        })
-      }
+      triggerPositive,
+      triggerNegative,
+      getPokemon
     }
   }
 }
